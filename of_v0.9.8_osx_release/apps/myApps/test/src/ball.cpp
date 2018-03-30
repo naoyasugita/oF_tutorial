d7cf5065dd1cf2e386b18282fcff013b8ef33f57
n_sugita <sugita6878@gmail.com>
2018-03-30 14:16:03 +0900

    fix file

---
+++ of_v0.9.8_osx_release/apps/myApps/test/src/ball.cpp
@@ -0,0 +1,9 @@
+//
+//  ball.cpp
+//  test
+//
+//  Created by 杉田尚弥 on 2018/03/30.
+//
+//
+
+#include <stdio.h>
---
+++ of_v0.9.8_osx_release/apps/myApps/test/src/ball.h
@@ -0,0 +1,13 @@
+//
+//  ball.h
+//  test
+//
+//  Created by 杉田尚弥 on 2018/03/30.
+//
+//
+
+#ifndef ball_h
+#define ball_h
+
+
+#endif /* ball_h */
